31f68d3aec1ca98dbbe61b4eecd02b2da9b7e990
pajitnov <alex@pajitnov.info>
2020-01-29 02:00:39 -0500

    import ui layer

---
+++ src/actions/index.js
@@ -0,0 +1,22 @@
+let nextTodoId = 0
+export const addTodo = text => ({
+  id: nextTodoId++,
+  text,
+  type: 'ADD_TODO'
+})
+
+export const setVisibilityFilter = filter => ({
+  filter,
+  type: 'SET_VISIBILITY_FILTER'
+})
+
+export const toggleTodo = id => ({
+  id,
+  type: 'TOGGLE_TODO'
+})
+
+export const VisibilityFilters = {
+  SHOW_ACTIVE: 'SHOW_ACTIVE',
+  SHOW_ALL: 'SHOW_ALL',
+  SHOW_COMPLETED: 'SHOW_COMPLETED'
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,27 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
+import rootReducer from './reducers';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { render } from 'react-dom';
+const store = createStore(rootReducer);
+import './index.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root') as HTMLElement
+render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
 );
+
 registerServiceWorker();
+
+
+
+
+
+//
+// const store = createStore(rootReducer)
+//
